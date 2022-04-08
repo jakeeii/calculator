6300946f1e3424f560d454f61b13b7b00dfd67fc
Jake <jakevisc@gmail.com>
2022-04-08 16:47:50 -0230

    Update buttons to display on the 'screen'
    Slightly updated the CSS file with minor graphic changes. Also updated
    the JS file to show the input on the calculator screen once a button is
    clicked. Logic is still not complete, the screen only shows the numbers
    but will not evaluate anything.

--- calc.js
+++ calc.js
@@ -1,3 +1,22 @@
+const clear = document.getElementById('clear')
+const deleteBtn = document.getElementById('delete')
+const display = document.getElementById('display')
+const one = document.getElementById('1')
+const two = document.getElementById('2')
+const three = document.getElementById('3')
+const four = document.getElementById('4')
+const five = document.getElementById('5')
+const six = document.getElementById('6')
+const seven = document.getElementById('7')
+const eight = document.getElementById('8')
+const nine = document.getElementById('9')
+const equal = document.getElementById('=')
+const minus = document.getElementById('-')
+const plus = document.getElementById('+')
+const divid = document.getElementById('÷')
+const multip = document.getElementById('x')
+
+
 function add(num1, num2) {
   return num1 + num2
 }
@@ -26,3 +45,17 @@ function operate(num1, num2, operator) {
   }
 }
 
+clear.addEventListener('click', () => display.textContent = "")
+one.addEventListener('click', () => display.textContent += '1')
+two.addEventListener('click', () => display.textContent += '2')
+three.addEventListener('click', () => display.textContent += '3')
+four.addEventListener('click', () => display.textContent += '4')
+five.addEventListener('click', () => display.textContent += '5')
+six.addEventListener('click', () => display.textContent += '6')
+seven.addEventListener('click', () => display.textContent += '7')
+eight.addEventListener('click', () => display.textContent += '8')
+nine.addEventListener('click', () => display.textContent += '9')
+plus.addEventListener('click', () => display.textContent += ' + ')
+minus.addEventListener('click', () => display.textContent += ' - ')
+multip.addEventListener('click', () => display.textContent += ' x ')
+divid.addEventListener('click', () => display.textContent += ' ÷ ')
