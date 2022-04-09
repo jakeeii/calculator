5563530428a87a10a2dae8c0a8ca07a669c6537f
Jake <jakevisc@gmail.com>
2022-04-09 16:38:16 -0230

    Create getResult function which calculates input
    I have added a getResult function which will take in the input entered
    by pressing the buttons, then compute each pair of numbers individually
    (for example, if the input is '12 + 8 - 10' it will first add 12 + 8 and
    then subtract 10 from that result). The function is dynamic and should
    work with any number of operations as long as there is at least two
    numbers and an operator. If this is not the case then the function should
    return 'ERROR'.

--- calc.js
+++ calc.js
@@ -1,6 +1,7 @@
 const clear = document.getElementById('clear')
 const deleteBtn = document.getElementById('delete')
 const display = document.getElementById('display')
+const zero = document.getElementById('0')
 const one = document.getElementById('1')
 const two = document.getElementById('2')
 const three = document.getElementById('3')
@@ -33,29 +34,80 @@ function divide(num1, num2) {
   return num1 / num2
 }
 
-function operate(num1, num2, operator) {
+function operate(num1, operator, num2) {
   if (operator === '+') {
     return add(num1, num2)
   } else if (operator === '-') {
     return subtract(num1, num2)
-  } else if (operator === '*') {
+  } else if (operator === '×') {
     return multiply(num1, num2)
   } else if (operator === '/' || operator === '÷') {
     return divide(num1, num2)
   }
 }
 
-clear.addEventListener('click', () => display.textContent = "")
-one.addEventListener('click', () => display.textContent += '1')
-two.addEventListener('click', () => display.textContent += '2')
-three.addEventListener('click', () => display.textContent += '3')
-four.addEventListener('click', () => display.textContent += '4')
-five.addEventListener('click', () => display.textContent += '5')
-six.addEventListener('click', () => display.textContent += '6')
-seven.addEventListener('click', () => display.textContent += '7')
-eight.addEventListener('click', () => display.textContent += '8')
-nine.addEventListener('click', () => display.textContent += '9')
-plus.addEventListener('click', () => display.textContent += ' + ')
-minus.addEventListener('click', () => display.textContent += ' - ')
-multip.addEventListener('click', () => display.textContent += ' x ')
-divid.addEventListener('click', () => display.textContent += ' ÷ ')
+function getResult(input) {
+  let nums = input.split(" ")
+  while (nums.length > 1) {
+    let result = operate(parseInt(nums.shift()), nums.shift(), parseInt(nums.shift()))
+    nums.unshift(result)
+  }
+  return nums.shift()
+}
+
+let input = ''
+
+clear.addEventListener('click', () => display.textContent = '')
+zero.addEventListener('click', () => display.textContent = '0')
+one.addEventListener('click', () => {
+  display.textContent += '1'
+  input += '1'
+})
+two.addEventListener('click', () => {
+  display.textContent += '2'
+  input += '2'
+})
+three.addEventListener('click', () => {
+  display.textContent += '3'
+  input += '3'
+})
+four.addEventListener('click', () => {
+  display.textContent += '4'
+  input += '4'
+})
+five.addEventListener('click', () => {
+  display.textContent += '5'
+})
+six.addEventListener('click', () => {
+  display.textContent += '6'
+  input += '6'
+})
+seven.addEventListener('click', () => {
+  display.textContent += '7'
+  input += '7'
+})
+eight.addEventListener('click', () => {
+  display.textContent += '8'
+  input += '8'
+})
+nine.addEventListener('click', () => {
+  display.textContent += '9'
+  input += '9'
+})
+plus.addEventListener('click', () => {
+  display.textContent += ' + '
+  input += ' + '
+})
+minus.addEventListener('click', () => {
+  display.textContent += ' - '
+  input += ' - '
+})
+multip.addEventListener('click', () => {
+  display.textContent += ' × '
+  input += ' × '
+})
+divid.addEventListener('click', () => {
+  display.textContent += ' ÷ '
+  input += ' ÷ '
+})
+
