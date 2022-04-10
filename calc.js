9dd2381f0d186645bbf3ff612ce051a076ebfb82
Jake <jakevisc@gmail.com>
2022-04-10 09:54:10 -0230

    Added helper functions for updating display
    I added the helper functions updateDisplay and clearDisplay to clean up
    my code. Also, pressing '=' now evaluates the input and returns the
    result. The getResult function was also updated such that if the display
    is 'ERROR', pressing a button will clear the display and then input the
    button pressed, letting users avoid having to manually clear the ERROR
    message. The getResult function was also updated to round the result, if
    necessary, to 5 decimal places so that the result does not flow off the
    screen.

--- calc.js
+++ calc.js
@@ -47,67 +47,48 @@ function operate(num1, operator, num2) {
 }
 
 function getResult(input) {
+  console.log(input)
   let nums = input.split(" ")
+  if (nums.filter(e => e).length < 3) {
+    display.textContent = 'ERROR'
+    return
+  }
   while (nums.length > 1) {
     let result = operate(parseInt(nums.shift()), nums.shift(), parseInt(nums.shift()))
     nums.unshift(result)
   }
-  return nums.shift()
+  display.textContent = nums[0].toFixed(5)
+  return nums.shift().toFixed(5)
 }
 
 let input = ''
 
-clear.addEventListener('click', () => display.textContent = '')
-zero.addEventListener('click', () => display.textContent = '0')
-one.addEventListener('click', () => {
-  display.textContent += '1'
-  input += '1'
-})
-two.addEventListener('click', () => {
-  display.textContent += '2'
-  input += '2'
-})
-three.addEventListener('click', () => {
-  display.textContent += '3'
-  input += '3'
-})
-four.addEventListener('click', () => {
-  display.textContent += '4'
-  input += '4'
-})
-five.addEventListener('click', () => {
-  display.textContent += '5'
-})
-six.addEventListener('click', () => {
-  display.textContent += '6'
-  input += '6'
-})
-seven.addEventListener('click', () => {
-  display.textContent += '7'
-  input += '7'
-})
-eight.addEventListener('click', () => {
-  display.textContent += '8'
-  input += '8'
-})
-nine.addEventListener('click', () => {
-  display.textContent += '9'
-  input += '9'
-})
-plus.addEventListener('click', () => {
-  display.textContent += ' + '
-  input += ' + '
-})
-minus.addEventListener('click', () => {
-  display.textContent += ' - '
-  input += ' - '
-})
-multip.addEventListener('click', () => {
-  display.textContent += ' × '
-  input += ' × '
-})
-divid.addEventListener('click', () => {
-  display.textContent += ' ÷ '
-  input += ' ÷ '
-})
+function clearDisplay() {
+  display.textContent = ''
+  input = ''
+}
 
+function updateDisplay(char) {
+  if (display.textContent === 'ERROR') {
+    clearDisplay();
+  }
+  display.textContent += char
+  input += char
+}
+
+clear.addEventListener('click', () => clearDisplay())
+zero.addEventListener('click', () => updateDisplay('0'))
+one.addEventListener('click', () => updateDisplay('1'))
+two.addEventListener('click', () => updateDisplay('2'))
+three.addEventListener('click', () => updateDisplay('3'))
+four.addEventListener('click', () => updateDisplay('4'))
+five.addEventListener('click', () => updateDisplay('5'))
+six.addEventListener('click', () => updateDisplay('6'))
+seven.addEventListener('click', () => updateDisplay('7'))
+eight.addEventListener('click', () => updateDisplay('8'))
+nine.addEventListener('click', () => updateDisplay('9'))
+plus.addEventListener('click', () => updateDisplay(' + '))
+minus.addEventListener('click', () => updateDisplay(' - '))
+multip.addEventListener('click', () => updateDisplay(' × '))
+divid.addEventListener('click', () => updateDisplay(' ÷ '))
+equal.addEventListener('click', () => getResult(input))
